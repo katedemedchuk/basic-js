51675deafff6fdc8d47e40d3ab0ae8d662b647c0
katedemedchuk <demedchuk.kate@gmail.com>
2020-03-21 19:39:07 +0300

    feat: add rec depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,13 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, res = 1, cur = 1) {
+      arr.forEach( (el) => {
+        if (Array.isArray(el)) {
+          cur++;
+          if (res < cur) res++;
+          res = Math.max(this.calculateDepth(el, res, cur), res);
+          cur--;
+        }
+      });  
+      return res;
     }
 };
