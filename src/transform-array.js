a4061f44b09c34fb1fa0b244d17136d6afea3462
katedemedchuk <demedchuk.kate@gmail.com>
2020-03-21 19:22:04 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,19 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) 
+        throw new Error('arr is not a valid Array')
+    
+    if(arr.length == 0) return []
+  
+    return arr.map((e, idx) => {
+    if (['--discard-next', '--discard-prev', '--double-next','--double-prev']
+    .includes(e)) return []
+
+    const ins = [e]
+    let iNext = idx+1
+    while (arr[iNext] == '--discard-next') iNext+=2
+    if (arr[idx-1]=='--discard-next' || arr[iNext]=='--discard-prev') ins.pop()
+    if (arr[idx-1]=='--double-next') ins.push(e)
+    if (arr[idx+1]=='--double-prev') ins.push(e)
+    return ins
+    }).flat()
 };
